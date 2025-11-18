cb056f4983e356c6b174f898d922d0ae9e683d02
Reihan Agam <reihanagam7@gmail.com>
2025-11-18 08:58:07 +0700

    update

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,16 +1,16 @@
-const express = require("express");
+const express = require('express');
 const Route = express.Router();
-const admin = require("../controllers/admin");
+const admin = require('../controllers/admin');
 
-Route.post("/user-management-store", admin.userManagementStore);
-Route.put("/user-management-update", admin.userManagementUpdate);
-Route.get("/user-management-list", admin.userManagementList);
-Route.get("/user-booking-list", admin.userBookingList);
-Route.get("/user-role-list", admin.userRoleList);
-Route.get("/service-list", admin.serviceList);
-Route.get("/service-store", admin.serviceStore);
-Route.put("/service-update", admin.serviceUpdate);
-Route.put("/update-booking-status", admin.updateBookingStatus);
-Route.get("/tech-schedule", admin.techSchedule);
+Route.post('/user-management-store', admin.userManagementStore);
+Route.put('/user-management-update', admin.userManagementUpdate);
+Route.get('/user-management-list', admin.userManagementList);
+Route.get('/user-booking-list', admin.userBookingList);
+Route.get('/user-role-list', admin.userRoleList);
+Route.get('/service-list', admin.serviceList);
+Route.post('/service-store', admin.serviceStore);
+Route.put('/service-update', admin.serviceUpdate);
+Route.put('/update-booking-status', admin.updateBookingStatus);
+Route.get('/tech-schedule', admin.techSchedule);
 
 module.exports = Route;
